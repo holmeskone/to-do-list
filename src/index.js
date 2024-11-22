ac0931dd2442705a332a8259cf73a4f06fd2f7a2
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-22 16:42:06 +0100

    Call createdProjects functions on click of project creation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import "./style.css"; // Import CSS styles 
+import createdProjects from "./projects-list";
 
-console.log('hello world')
-
-const content = document.getElementById('content');
-const tester = document.createElement('p');
-tester.textContent = 'Hello, this is a test';
-content.appendChild(tester);
+const button = document.getElementById('create-project');
+button.addEventListener("click", createdProjects);

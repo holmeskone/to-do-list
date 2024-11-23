5e2ea512d02db45b8ef7a5e3d20e26f877f6d601
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-23 14:11:43 +0100

    Removed file

--- src/projects-list.js
+++
@@ -1,8 +0,0 @@
-export default function createdProjects(){
-    const projectBar = document.getElementById('created-projects');
-    const projectSection = document.createElement('div');
-    const projectName = document.createElement('h3');
-    projectName.innerHTML = document.getElementById('project-name').value
-    projectSection.appendChild(projectName);
-    projectBar.appendChild(projectSection);
-}

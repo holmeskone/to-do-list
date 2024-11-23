8b459db4c4f2aa16f726474d263018102460faeb
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-23 14:06:48 +0100

    Added ID's and a delete project option

--- src/project-creation.js
+++ src/project-creation.js
@@ -1,4 +1,5 @@
-import {Project} from "./project.js" 
+import {Project} from "./project.js"
+import { deleteProject } from "./delete-project";
 
 export function setUpProjectCreation(){
     const createProjectButton = document.getElementById('create-project-button');
@@ -10,12 +11,18 @@ export function setUpProjectCreation(){
 function createProject(){
     const projectBar = document.getElementById('created-projects');
     const projectSection = document.createElement('div');
+    projectSection.id = 'project-1'
     const projectNameDisplay = document.createElement('h3');
+    projectNameDisplay.id = 'project-name'
+    const deleteProject = document.createElement('button');
+    deleteProject.id = 'delete';
+    deleteProject.textContent = '❌';
     const projectName = document.getElementById('input-project-name').value
     projectNameDisplay.innerHTML = projectName;
     const newProject =  new Project(projectName);
     console.log('New project created:', newProject);
-    projectSection.appendChild(projectNameDisplay);
+    projectSection.append(projectNameDisplay,deleteProject);
     projectBar.appendChild(projectSection);
+
 }
 

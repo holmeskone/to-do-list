b656cf3812f269e416516cde7dd2dde29efba6bd
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-23 14:07:55 +0100

    Added class creator

---
+++ src/project.js
@@ -0,0 +1,10 @@
+export class Project{
+    constructor(name){
+        this.name = name,
+        this.createdAt = new Date();
+    }
+
+    getDetails(){
+        return `Project: ${this.name}, Created At: ${this.createdAt}`
+    }
+}

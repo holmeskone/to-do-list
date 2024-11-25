9c782803393b547a6dcb1af1c798cedb3be73181
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-25 15:42:52 +0100

    Added dynamic project name

--- src/project-creation.js
+++ src/project-creation.js
@@ -3,13 +3,13 @@ import {Project} from "./project.js"
 export function createProject(){
     const projectBar = document.getElementById('created-projects');
     const projectSection = document.createElement('div');
-    projectSection.id = 'project-1'
     const projectNameDisplay = document.createElement('h3');
     projectNameDisplay.id = 'project-name'
     const deleteProject = document.createElement('button');
     deleteProject.id = 'delete';
     deleteProject.textContent = '❌';
     const projectName = document.getElementById('input-project-name').value
+    projectSection.id = `${projectName}`
     projectNameDisplay.innerHTML = projectName;
     const newProject =  new Project(projectName);
     console.log('New project created:', newProject);

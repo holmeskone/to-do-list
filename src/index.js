214fbc24b8bd29e30e58c898b7cff029a05e418b
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-25 13:37:32 +0100

    Added event listener on click

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import {setUpProjectCreation} from "./project-creation";
-import { deleteProjectCreation } from "./delete-project";
+import {delegateProjectCreation} from "./project";
+
 
 // Initialize the application
 document.addEventListener('DOMContentLoaded', () => {
     console.log('App initialized');
-    setUpProjectCreation(); // Delegate setup for the "Create Project" button
 });
 
+
+// Initialize the application
+document.addEventListener('click', () => {
+    delegateProjectCreation(); // Delegate setup for the "Create Project" button
+});

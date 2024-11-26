861e9d6bbb70016a4b661e579cd5cbbf214a6b86
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-26 10:52:56 +0100

    Add all elements id for the form and call the create function with correct variables

--- src/todo/manage-to-dos.js
+++ src/todo/manage-to-dos.js
@@ -2,9 +2,12 @@ import { createToDo } from "./create-todo";
 
 export function managetodo(){
     const addToDo = document.getElementById('create-todo');
-    // const titleToDo = document.getElementById('title');
-    // const descriptionToDo = document.getElementById('description');
+    const titleToDo = document.getElementById('title').value;
+    const dateToDo = document.getElementById('date').value;
+    const descriptionToDo = document.getElementById('description').value;
+    const priorityToDo = document.getElementById('priorities').value;
+    const notesToDo = document.getElementById('notes').value;
     if (addToDo){
-        addToDo.addEventListener('click', createToDo('titleToDo','test'));
+        addToDo.addEventListener('click', createToDo(titleToDo,dateToDo,descriptionToDo,priorityToDo,notesToDo));
     }
 }

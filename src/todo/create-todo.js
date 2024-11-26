1e2cc2335c2f181d7670acfd4b993f179cf4aabb
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-26 14:45:23 +0100

    Import currentProject variable from create a project, create a new Todo and push that to do, to the todos array constructor, finally print an overview to ensure everything is smooth

--- src/todo/create-todo.js
+++ src/todo/create-todo.js
@@ -1,6 +1,10 @@
 import { Todo } from "./class-todo";
+import { currentProject } from "../project/create-project";
+
 
 export function createToDo(title, date, description, priority, notes){
-    const newToDo = new Todo(title, date, description, priority, notes);
-    console.log('New project created:', newToDo);
+    const newTodo = new Todo(title, date, description, priority, notes);
+    currentProject.todos.push(newTodo); // currentProject is the array with all the information about the project
+    console.log('A new todo was created for', currentProject.name, '. The todo is:', newTodo);
+    console.log('The updated overview of the project is', currentProject);
 }

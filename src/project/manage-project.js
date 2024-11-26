044bc355aa7ac32f80f0c6b561da3ebb38e4ea6e
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-26 15:07:11 +0100

    Add default function manager that redirects if user has recently opened app

--- src/project/manage-project.js
+++ src/project/manage-project.js
@@ -1,5 +1,6 @@
 import { createProject } from "./create-project";
 import { deleteProject } from "./delete-project";
+import { createDefaultProject } from "./create-project";
 
 export function delegateProjectOperations(){
     const createProjectButton = document.getElementById('create-project-button');
@@ -10,6 +11,9 @@ export function delegateProjectOperations(){
     if (deleteProjectButton){
         const parentID = deleteProjectButton.parentElement // find the parentElement of the clicked delete button
         deleteProjectButton.addEventListener('click', () => deleteProject(parentID)); //Added  => because if not the function is called twice
-
     }
+}
+
+export function defaultProjectOperation(){
+    createDefaultProject()
 }

64ae3b58755c2b20fdbdac86b640a4df6ff3cfb1
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-27 12:35:28 +0100

    Import display function and call function based on click in the tab section

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
+import "./style.css"; // Import CSS styles 
 import {delegateProjectOperations} from "./project/manage-project";
 import { managetodo } from "./todo/manage-to-dos";
 import { defaultProjectOperation } from "./project/manage-project";
-
+import { displayTab } from "./todo/view-todo";
 
 // Initialize the application
 document.addEventListener('DOMContentLoaded', () => {
     console.log('App initialized');
     defaultProjectOperation();
 });
-
 
 // Call when there's a click on the project section
 const projectSection = document.getElementById('project-section')
@@ -20,4 +20,10 @@ projectSection.addEventListener('click', () => {
 const toDoSection = document.getElementById('create-todo-button');
 toDoSection.addEventListener('click', () => {
     managetodo();
+});
+
+//Call when tabs are clicked
+const tabContent = document.getElementById("todo-tab");
+tabContent.addEventListener('click', (e) => {
+    displayTab(e.target.id)
 });

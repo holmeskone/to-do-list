be35bdd77587f52c1cc347e3a18a9a37fd818934
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-28 14:48:52 +0000

    Added comments to understand what currentProject is and how we are linking the todo to the project and viceversa

--- src/todo/create-todo.js
+++ src/todo/create-todo.js
@@ -3,10 +3,13 @@ import { currentProject } from "../project/create-project";
 import { displayTodo } from "./view-todo";
 
 
-export function createToDo(title, date, description, priority, notes){
-    const newTodo = new Todo(title, date, description, priority, notes);
-    currentProject.todos.push(newTodo); // currentProject is the array with all the information about the project
+export function createToDo(title, date, description, priority, notes, project){
+    const newTodo = new Todo(title, date, description, priority, notes, project); // It creates a new instance of the class To Do
+    currentProject.todos.push(newTodo); // currentProject is the class of Object Project, here we get it's property to do's and push our recently made todo instance into it the project. 
     console.log('A new todo was created for', currentProject.name, '. The todo is:', newTodo);
     console.log('The updated overview of the project is', currentProject);
     displayTodo(newTodo);
 }
+
+
+//Curent project has to be the SELECTED project not the most recent.

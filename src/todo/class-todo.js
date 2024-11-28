b0c06da3f3a0da4a00f9d3a1bbae9ad1d92a5cf4
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-28 14:39:52 +0000

    Add project as property of todo, project will link the todo to it's corresponding project

--- src/todo/class-todo.js
+++ src/todo/class-todo.js
@@ -1,16 +1,17 @@
 export class Todo{
-    constructor(title, date, description, priority, notes){
+    constructor(title, date, description, priority, notes, project){
         this.title = title,
         this.date = date,
         this.description = description,
         this.priority = priority,
         this.notes = notes,
+        this.project = project,
         this.createdAt = new Date();
     }
 
     getDetails(){
         console.log('Class created');
-        return `To Do: ${this.title}, Date: ${this.date} Description: ${this.description}, Priority: ${this.priority}, Notes: ${this.notes}, Created At: ${this.createdAt}`
+        return `To Do: ${this.title}, Date: ${this.date} Description: ${this.description}, Priority: ${this.priority}, Notes: ${this.notes}, Project: ${this.project}, Created At: ${this.createdAt}`
     } 
     
 }

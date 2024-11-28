2968505c758d2e0ef3ec9414e6d0087edcfbc618
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-28 14:53:29 +0000

    Added select project as an option, and included a variable that is the section where they clicked, if that section is equal to the created variable then we will perform specific functions

--- src/project/manage-project.js
+++ src/project/manage-project.js
@@ -1,18 +1,24 @@
 import { createProject } from "./create-project";
 import { deleteProject } from "./delete-project";
 import { createDefaultProject } from "./create-project";
+import { selectProject } from "./select-project";
 
-export function delegateProjectOperations(){
-    const createProjectButton = document.getElementById('create-project-button');
-    const deleteProjectButton = document.getElementById('delete');
-    if(createProjectButton){
-        createProjectButton.addEventListener('click', createProject);
+export function delegateProjectOperations(section){
+    const createProjectButton = document.getElementById('create-project-button'); // variable for the add project button
+    const deleteProjectButtonClass = 'delete-project' // variable for the delete project button ❌
+    const projectClass = 'project'; // variable for the project, specifically when we select it
+    if(section === createProjectButton){ // if you click add project send to create project function
+        createProject();
+    };
+
+    if (section.className === deleteProjectButtonClass){ // if you click delete project then send to that file 
+        deleteProject(section.parentElement); //Added  => because if not the function is called twice
+    };
+
+    if (section.parentElement.className === projectClass) { // if you click on the project then redirect to selectProject
+            selectProject(section, section.parentElement.id)
+        };
     }
-    if (deleteProjectButton){
-        const parentID = deleteProjectButton.parentElement // find the parentElement of the clicked delete button
-        deleteProjectButton.addEventListener('click', () => deleteProject(parentID)); //Added  => because if not the function is called twice
-    }
-}
 
 export function defaultProjectOperation(){
     createDefaultProject()

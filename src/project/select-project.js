e9a28ee29de3ddaf12e9636236d028c6db8d8a59
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-11-29 20:47:46 +0000

    Change variable from className to query selector, then for each value returned loop through them and if they have the attribute data project that is the same value as the currently selected project then change their display value to block, this way only the todos of the selectedProjects are displayed

--- src/project/select-project.js
+++ src/project/select-project.js
@@ -3,12 +3,12 @@ import { allProjects } from "./create-project";
 let selectProjectName = null; 
 export function selectProject(event, projectName) {
     // Declare all variables
-    var i, project, todo, v;
+    var i, project, todos;
   
-    // Get all elements with class="todo" and hide them
-    todo = document.getElementsByClassName("todo");
-    for (i = 0; i < todo.length; i++) {
-      todo[i].style.display = "none";
+    // Get all elements with class="todo" and hide them, then show the specific todos of a selected project
+    todos = document.querySelectorAll(".todo");
+    for (i = 0; i < todos.length; i++) {
+      todos[i].style.display = "none";
     }
 
     //Fix that this none is only applied to the div of the todo's and not the whole todo-block
@@ -19,12 +19,17 @@ export function selectProject(event, projectName) {
         project[i].className = project[i].className.replace(" selected", "");
     }
   
-    // Show the selected project and to do list, and add an "active" class to the button that opened the tab
-    document.getElementById(projectName).style.display = "block";
+
+    todos.forEach(todo => {
+      if (todo.hasAttribute('data-project') && todo.getAttribute('data-project') === event.innerHTML) {
+          todo.style.display = 'block';
+      }
+  });
+
     event.parentElement.className += " selected";
     selectProjectName = event.innerHTML;
-    console.log('Selected Project: ', selectProjectName);
-    console.log('Selected Project: ', allProjects.get(event.innerHTML));
+    // console.log('Selected Project: ', selectProjectName);
+    // console.log('Selected Project: ', allProjects.get(event.innerHTML));
   }
 
   export {selectProjectName} ;

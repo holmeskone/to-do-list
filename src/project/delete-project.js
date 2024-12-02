eebebe9ed5469ac9a3a22738737a38f3505fe1bc
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-12-02 10:15:30 +0000

    Add ability to filter through data-project field and delete all todos when you want to delete the project

--- src/project/delete-project.js
+++ src/project/delete-project.js
@@ -1,3 +1,14 @@
+import { allProjects } from "./create-project";
 export function deleteProject(id){
+    let i;
+    let toDos = document.querySelectorAll(`[data-project=${id.firstChild.innerHTML}]`)
+
+    console.log(toDos)
+
+    for(i=0; i<toDos.length; i++){
+        toDos[i].remove()
+        console.log('this has been deleted',toDos[i])
+    }
     id.remove();
 }
+    

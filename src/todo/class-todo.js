d6ca804a6077631acc94e6f06664a890ca9507ed
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-12-11 11:34:01 +0000

    Add status as a constructor of Todo

--- src/todo/class-todo.js
+++ src/todo/class-todo.js
@@ -1,17 +1,18 @@
 export class Todo{
-    constructor(title, date, description, priority, notes, project){
+    constructor(title, date, description, priority, notes, project, status){
         this.title = title,
         this.date = date,
         this.description = description,
         this.priority = priority,
         this.notes = notes,
         this.project = project,
+        this.status = status,
         this.createdAt = new Date();
     }
 
     getDetails(){
         console.log('Class created');
-        return `To Do: ${this.title}, Date: ${this.date} Description: ${this.description}, Priority: ${this.priority}, Notes: ${this.notes}, Project: ${this.project}, Created At: ${this.createdAt}`
+        return `To Do: ${this.title}, Date: ${this.date} Description: ${this.description}, Priority: ${this.priority}, Notes: ${this.notes}, Project: ${this.project}, Status: ${this.status}, Created At: ${this.createdAt}`
     } 
     
 }

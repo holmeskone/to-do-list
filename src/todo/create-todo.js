53cce783c0f325bf7f371c87ba365d1dd627dd74
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-12-13 16:37:29 +0000

    Store each todo to its respective key in localStorage

--- src/todo/create-todo.js
+++ src/todo/create-todo.js
@@ -12,7 +12,17 @@ export function createToDo(title, date, description, priority, notes, project,st
     const projectName = allProjects.get(selectProjectName);
     console.log(projectName);
     projectName.addTodo(newTodo); // currentProject is the class of Object Project, here we get it's property to do's and push our recently made todo instance into it the project. 
-    // console.log('A new todo was created for', Project.name, '. The todo is:', project.getDetails());
+
+    const localStoredProject = localStorage.getItem(selectProjectName)
+    console.log(`This is localStoredProject`,localStoredProject)
+    const storedProject =  JSON.parse(localStoredProject);
+    storedProject.todos.push(newTodo);
+    const updatedStoredProject =  JSON.stringify(storedProject);
+    localStorage.setItem(`${selectProjectName}`,updatedStoredProject);
+    console.log(JSON.parse(localStorage.getItem(`${selectProjectName}`)));
+    // console.log(allProjects.get("Default Project"))
+    // localStorage.setItem('Default Project' , JSON.stringify(allProjects.get(`${projectName}`)))
+    // console.log(localStorage.getItem(`${projectName}`));
     displayTodo(projectName);
 }
 

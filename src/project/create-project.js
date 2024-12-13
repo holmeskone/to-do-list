a6a76b209988172760d5aab8f2ba049a62d8b0f8
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-12-13 16:34:16 +0000

    Set project in local storage and then only create the default project but don't display, now display is handled by its own file, finally once the project is created and set in localstorage we call the new displayProject function

--- src/project/create-project.js
+++ src/project/create-project.js
@@ -1,5 +1,6 @@
 import {Project} from "./class-project.js"
 import { selectProject } from "./select-project.js";
+import { displayProject } from "./display-project.js";
 
 let currentProject = null;
 const allProjects = new Map(); // We use Map to make it efficient to retrieve a project by its name.
@@ -28,33 +29,42 @@ export function createProject(){
     projectSection.append(projectNameDisplay,deleteProject);
     projectBar.appendChild(projectSection);
     input.value = "";
-    selectProject(projectSection.firstElementChild,projectSection.id); // Make the latest project as the currently selected, the variables are to align with the selected function.
+    selectProject(projectSection.firstElementChild,projectSection.id); // Make the latest project as the currently selected, the variables are to align with the selected function
+    console.log(allProjects.get(projectName))
+    localStorage.setItem(projectName , JSON.stringify(allProjects.get(`${projectName}`))) // This sets the project name as the key in the localStorage, as value it includes the Map().
+    console.log(localStorage.getItem(`${projectName}`));
 }
 
 
 export function createDefaultProject(){
-    const projectBar = document.getElementById('created-projects');
-    const projectSection = document.createElement('div');
-    const projectNameDisplay = document.createElement('h3');
-    const deleteProject = document.createElement('button');
+    // const projectBar = document.getElementById('created-projects');
+    // const projectSection = document.createElement('div');
+    // const projectNameDisplay = document.createElement('h3');
+    // const deleteProject = document.createElement('button');
     const projectName = 'Default Project'
     // Create a new project
     const newProject = new Project(projectName);
     allProjects.set(projectName, newProject);
     console.log('All projects: ', allProjects);
-    projectNameDisplay.id = `${projectName}`
-    projectNameDisplay.className = `${projectName}-title`
-    deleteProject.id = `${projectName}-delete`;
-    deleteProject.className = 'delete-project';
-    deleteProject.textContent = '❌';
-    projectSection.id = `${projectName}-section`;
-    projectSection.className = 'project';
-    projectNameDisplay.innerHTML = projectName;
+    // projectNameDisplay.id = `${projectName}`
+    // projectNameDisplay.className = `${projectName}-title`
+    // deleteProject.id = `${projectName}-delete`;
+    // deleteProject.className = 'delete-project';
+    // deleteProject.textContent = '❌';
+    // projectSection.id = `${projectName}-section`;
+    // projectSection.className = 'project';
+    // projectNameDisplay.innerHTML = projectName;
     console.log('New project created:', newProject);
-    projectSection.append(projectNameDisplay,deleteProject);
-    projectBar.appendChild(projectSection);
-    selectProject(projectSection.firstElementChild,projectSection.id); //Make default project as selected one. 
+    // projectSection.append(projectNameDisplay,deleteProject);
+    // projectBar.appendChild(projectSection);
+    // selectProject(projectSection.firstElementChild,projectSection.id); //Make default project as selected one.
+    // console.log(allProjects.get("Default Project"))
+    localStorage.setItem('Default Project' , JSON.stringify(allProjects.get(`${projectName}`))) // This sets the project name as the key in the localStorage, as value it includes the Map().
+    // console.log(localStorage.getItem(`${projectName}`));
+    displayProject(projectName);
 }
+
+
 
 
 // Export the current SELECTED project to the to do creation file. 

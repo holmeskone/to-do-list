3f575ed0d9e5b183700fce413004e73bfe5df694
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-12-13 16:35:40 +0000

    If the local storage is empty then create the default project, however if its not, loop through the keys and call displayProject for each key so it displays each project previously created

--- src/project/manage-project.js
+++ src/project/manage-project.js
@@ -2,14 +2,38 @@ import { createProject } from "./create-project";
 import { deleteProject } from "./delete-project";
 import { createDefaultProject } from "./create-project";
 import { selectProject } from "./select-project";
+import { displayProject, displaySavedProject } from "./display-project";
+
+
+// export function retrieveStorage(){
+//     // Loop through all keys in localStorage
+//     for (let i = 0; i < localStorage.length; i++) {
+
+//     if (localStorage.length === 0) {
+//         defaultProjectOperation()
+//     }
+//     // Get the key at the current index
+//     const key = localStorage.key(i);
+  
+//     // Retrieve the value associated with the key
+//     const dataString = localStorage.getItem(key);
+  
+//     // Parse the JSON string into a JavaScript object
+//     const data = JSON.parse(dataString);
+//     console.log(`data is:`, data)
+//     // Log the key and its data to the console
+//     console.log(`Key: ${key}, Data:`, data);
+
+//   }
+// }
+
 
 export function delegateProjectOperations(section){
-    console.log('This is a section',section.parentElement.firstChild.innerHTML);
     const createProjectButton = document.getElementById('create-project-button'); // variable for the add project button
     const deleteProjectButtonClass = 'delete-project' // variable for the delete project button ❌
     const projectClass = 'project'; // variable for the project, specifically when we select it
     if(section === createProjectButton){ // if you click add project send to create project function
-        createProject();
+        createProject();    
     };
 
     if (section.className === deleteProjectButtonClass){ // if you click delete project then send to that file 
@@ -22,5 +46,24 @@ export function delegateProjectOperations(section){
     }
 
 export function defaultProjectOperation(){
-    createDefaultProject()
+    if (localStorage.length === 0) {
+        createDefaultProject();
+    }
+    else{
+            // Loop through all keys in localStorage
+    for (let i = 0; i < localStorage.length; i++) {
+        // Get the key at the current index
+        const key = localStorage.key(i);
+        // Retrieve the value associated with the key
+        const dataString = localStorage.getItem(key);
+        
+        // Parse the JSON string into a JavaScript object
+        const data = JSON.parse(dataString);
+        console.log(`data is:`, data)
+        // Log the key and its data to the console
+        console.log(`Key: ${key}, Data:`, data);
+        displayProject(`${key}`);
+
+        }
+    }
 }

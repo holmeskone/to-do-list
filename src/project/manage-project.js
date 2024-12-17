28b451431e5f6d18b9dbefb79df8b83a2be119bd
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-12-17 10:58:09 +0100

    Removed commented out section

--- src/project/manage-project.js
+++ src/project/manage-project.js
@@ -5,29 +5,6 @@ import { selectProject } from "./select-project";
 import { displayProject, displaySavedProject } from "./display-project";
 
 
-// export function retrieveStorage(){
-//     // Loop through all keys in localStorage
-//     for (let i = 0; i < localStorage.length; i++) {
-
-//     if (localStorage.length === 0) {
-//         defaultProjectOperation()
-//     }
-//     // Get the key at the current index
-//     const key = localStorage.key(i);
-  
-//     // Retrieve the value associated with the key
-//     const dataString = localStorage.getItem(key);
-  
-//     // Parse the JSON string into a JavaScript object
-//     const data = JSON.parse(dataString);
-//     console.log(`data is:`, data)
-//     // Log the key and its data to the console
-//     console.log(`Key: ${key}, Data:`, data);
-
-//   }
-// }
-
-
 export function delegateProjectOperations(section){
     const createProjectButton = document.getElementById('create-project-button'); // variable for the add project button
     const deleteProjectButtonClass = 'delete-project' // variable for the delete project button ❌

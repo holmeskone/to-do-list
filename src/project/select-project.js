de16467501e365eab488a3371576c39e4a29b370
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-12-17 20:39:48 +0100

    Make it iterate through todos and display the div if the attribute is the same as the name of the selected project

--- src/project/select-project.js
+++ src/project/select-project.js
@@ -3,33 +3,28 @@ import { allProjects } from "./create-project";
 let selectProjectName = null; 
 export function selectProject(event, projectName) {
     // Declare all variables
-    var i, project, todos;
-  
-    // Get all elements with class="todo" and hide them, then show the specific todos of a selected project
-    todos = document.querySelectorAll(".todo");
-    for (i = 0; i < todos.length; i++) {
-      todos[i].style.display = "none";
-    }
-
+    let i, project;
     //Fix that this none is only applied to the div of the todo's and not the whole todo-block
   
     // Get all elements with class="project" and remove the class "selected"
-    project = document.getElementsByClassName("project");
-    for (i = 0; i < project.length; i++) {
-        project[i].className = project[i].className.replace(" selected", "");
-    }
-  
-
-    todos.forEach(todo => {
-      if (todo.hasAttribute('data-project') && todo.getAttribute('data-project') === event.innerHTML) {
-          todo.style.display = 'block';
-      }
-  });
-
-    event.parentElement.className += " selected";
-    selectProjectName = event.innerHTML;
-    // console.log('Selected Project: ', selectProjectName);
-    // console.log('Selected Project: ', allProjects.get(event.innerHTML));
+  project = document.getElementsByClassName("project");
+  for (i = 0; i < project.length; i++) {
+      project[i].className = project[i].className.replace(" selected", "");
   }
+  
+  //Get all todos and iterate through them, if the todo has an attribute called data-project that is the same as the selected project then display the todo, if not hide it.
+todos.forEach(todo => {
+  console.log(todo);
+  if (todo.hasAttribute('data-project') && todo.getAttribute('data-project') != event.innerHTML) {
+    todo.style.display = "none";
+    }
+  else if (todo.hasAttribute('data-project') && todo.getAttribute('data-project') === event.innerHTML) {
+    todo.style.display = "block";
+    }
+  });
+//Add selected to the clocked event. 
+  event.parentElement.className += " selected";
+  selectProjectName = event.innerHTML;
+}
 
   export {selectProjectName} ;

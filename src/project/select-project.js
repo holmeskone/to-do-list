a13f65ffa617dfdf1f7b068dad8b5c83d86ddc78
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-12-17 20:44:43 +0100

    Add query selector for todos, so it get's all of them

--- src/project/select-project.js
+++ src/project/select-project.js
@@ -2,27 +2,26 @@ import { allProjects } from "./create-project";
 
 let selectProjectName = null; 
 export function selectProject(event, projectName) {
-    // Declare all variables
-    let i, project;
-    //Fix that this none is only applied to the div of the todo's and not the whole todo-block
-  
-    // Get all elements with class="project" and remove the class "selected"
+  // Declare all variables
+  let i, project;
+  // Get all elements with class="project" and remove the class "selected"
   project = document.getElementsByClassName("project");
   for (i = 0; i < project.length; i++) {
       project[i].className = project[i].className.replace(" selected", "");
   }
   
-  //Get all todos and iterate through them, if the todo has an attribute called data-project that is the same as the selected project then display the todo, if not hide it.
-todos.forEach(todo => {
-  console.log(todo);
-  if (todo.hasAttribute('data-project') && todo.getAttribute('data-project') != event.innerHTML) {
-    todo.style.display = "none";
-    }
-  else if (todo.hasAttribute('data-project') && todo.getAttribute('data-project') === event.innerHTML) {
-    todo.style.display = "block";
-    }
-  });
-//Add selected to the clocked event. 
+  let todos = document.querySelectorAll(".todo");
+  //Get all todos and iterate through them, if the todo has an attribute called data-project that is the same as the selected project then display the todo, if not hide
+  todos.forEach(todo => {
+    console.log(todo);
+    if (todo.hasAttribute('data-project') && todo.getAttribute('data-project') != event.innerHTML) {
+      todo.style.display = "none";
+      }
+    else if (todo.hasAttribute('data-project') && todo.getAttribute('data-project') === event.innerHTML) {
+      todo.style.display = "block";
+      }
+    });
+
   event.parentElement.className += " selected";
   selectProjectName = event.innerHTML;
 }
